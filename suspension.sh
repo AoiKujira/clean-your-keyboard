5a20c36ae23ce601efa61620e9a063ee5b4ed637
Aoi Kujira <farzamzohdi@gmail.com>
2023-09-13 12:27:48 +0200

    Add suspension script, documentation and README.md

---
+++ suspension.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# =========================
+# xinput(1)
+
+# NAME
+#        xinput - utility to configure and test X input devices
+
+# SYNOPSIS
+#        xinput [COMMAND] [OPTIONS] [DEVICE]
+
+#        reattach slave master
+#                Reattach slave to master.
+
+#        float slave
+#                Remove slave from its current master device.
+# ========================
+
+#!/bin/bash
+
+# Define the list of IDs
+# u_ids=(10 12 11 13 14 6 7 8 9)
+
+# Iterate over the IDs and disable them
+for u_id in "${u_ids[@]}"; do
+    xinput disable "$u_id"
+done
+
+seconds=30
+# start timer
+while [ $seconds -gt 0 ]; do
+    echo "Remaining time: $seconds seconds"
+    sleep 1
+    (( seconds-- ))
+done
+
+echo "Time's up!"
+
+# Iterate over the IDs and enable them
+for u_id in "${u_ids[@]}"; do
+    xinput enable "$u_id"
+done
+
